64e7736b7d5a03e5aff698fbbaac16dc04c41cec
famllr <145350576+famllr@users.noreply.github.com>
2023-11-27 14:14:47 +0100

    Update blatt_4_7a.py

--- blatt_4_7a.py
+++ blatt_4_7a.py
@@ -2,6 +2,6 @@ import pandas as pd
 import plotly.express as px 
 
 # Daten einlesen
-data = pd.read_csv('https://github.com/corinalombris/Projekt_VIS_ISc20tzC/blob/main/table_1.csv', delimiter=';')
+data = pd.read_csv('table_1.csv', delimiter=';')
 
 
